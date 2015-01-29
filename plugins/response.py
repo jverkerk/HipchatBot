42e721f546fb7e1e463e5c329c32beba9ab1aece
jverkerk <verkerkmeister@gmail.com>
2015-01-29 14:13:17 -0500

    adding more responses

--- plugins/response.py
+++ plugins/response.py
@@ -5,14 +5,15 @@ class ResponsePlugin(WillPlugin):
 
     @respond_to("make me a sandwich")
     def sandwich(self, message):
-        """make me a sandwich"""
+        """make me a sandwich: I can make you a sandwich"""
         self.say("http://blogs.plos.org/obesitypanacea/files/2014/10/sandwich.jpg", message=message)
 
-#    @respond_to("^hi")
-#    def hi(self, message):
-#        """hi: I know how to say hello!"""
-#        self.reply(message, "hello!")
+    @respond_to("make me a pie")
+    def pie(self, message):
+        """make me a pie: I know what pie is."""
+        self.reply(message, "http://www.csn.edu/uploadedfiles/academics/divisions/science_and_math/math/images/potd_pi-pie.jpg")
 
-#    @respond_to("^hello$")
-#    def hello(self, message):
-#        self.reply(message, "hi!")
+    @respond_to("rm -rf.*")
+    def rm(self, message):
+        """rm -rf: I can erase it all"""
+        self.reply(message, "Erasing everything.. Say bye bye to your Production environment (boom)")
